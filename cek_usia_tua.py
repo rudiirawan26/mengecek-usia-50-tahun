992a35525e39bb642101bae260639b703a67d085
RUDI IRAWAN <50778467+rudiirawan26@users.noreply.github.com>
2021-01-15 20:11:00 +0700

    Create cek_usia_tua.py

---
+++ cek_usia_tua.py
@@ -0,0 +1,7 @@
+# # Contoh output:
+# Nama anda Soleh, anda berusia 15 tahun. Pada tahun 2054 anda akan berusia 50 tahun.
+
+nama = input("Masukan nama anda: ")
+umur = int(input("Masukan umur anda: "))
+rumus = umur + 2021
+print(f"Nama anda {nama}, anda berusia {umur}. Pada tahun {rumus} anda akan berusia 50 Tahun")
